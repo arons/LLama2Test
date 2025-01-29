ad26f40a736901378bbddbb70b0c56c124dca218
Renzo Dani <reda@logobject.ch>
2025-01-29 10:13:46 +0100

    jdk 21

--- src/java/ch/arons/llama/Transformer.java
+++ src/java/ch/arons/llama/Transformer.java
@@ -1,8 +1,8 @@
 package ch.arons.llama;
 
 import java.io.IOException;
+import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
-import java.lang.foreign.SegmentScope;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.FileChannel;
@@ -14,15 +14,17 @@ public final class Transformer {
     final Weights weights; // the weights of the model
     final RunState state; // buffers for the "wave" of activations in the forward pass
     // some more state needed to properly clean up the memory mapping (sigh)
-    final SegmentScope memoryScope; // scope of the memory mapping
+    final Arena memoryArena; // scope of the memory mapping
     final MemorySegment data; // memory mapped data pointer
     final long file_size; // size of the checkpoint file in bytes
 
     Transformer(String checkpoint_path) throws IOException {
-        try (FileChannel fileChannel = FileChannel.open(Paths.get(checkpoint_path), StandardOpenOption.READ)) {
+        try (FileChannel fileChannel = FileChannel.open(Paths.get(checkpoint_path), StandardOpenOption.READ);
+             Arena arena = Arena.ofAuto()) { // Use Arena.ofAuto() for automatic memory management
+            
             this.file_size = fileChannel.size();
-            this.memoryScope = SegmentScope.auto();
-            MemorySegment mappedFile = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, this.file_size, this.memoryScope);
+            this.memoryArena = arena;
+            MemorySegment mappedFile = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, this.file_size, arena);
             this.data = mappedFile;
             int configSize = 7 * Integer.BYTES;
             // read in the config header
